bb43e7e493275d830fc1a2a3fa97bdec5c6aa4b4
Hamza Alsakkal <130228445+HamzaAlsakkal@users.noreply.github.com>
2025-10-06 19:23:24 +0300

    Remove backend folder - focus on frontend-only implementation

--- POS_System_backend/src/purchase_items/dto/update-purchaseItems.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreatePurchaseItemsDto } from "./create-purchaseItems.dto";
-
-
-export class UpdatePurchaseItemsDto extends PartialType(CreatePurchaseItemsDto) {}
--- POS_System_backend/src/purchases/dto/update-purchases.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-import { CreatePurchasesDto } from "./create-purchases.dto";
-
-
-export class UpdatePurchasesDto extends PartialType(CreatePurchasesDto) {}
